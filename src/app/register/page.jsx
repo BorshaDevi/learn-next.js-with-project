f8ce019e84d4b4c8626f945514c082f1b7f8a489
unknown <borshadevi27@gmail.com>
2025-03-02 12:30:29 +0600

    added text

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -12,6 +12,7 @@ import {
   FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
+import Link from 'next/link'
 
 
 const formSchema=z.object({
@@ -37,7 +38,7 @@ const Register=()=>{
   }
     
     return(
-        <div className='min-h-screen w-full bg-gradient-to-r from-gray-100 to-gray-200'>
+        <div className='min-h-screen w-full flex flex-col md:flex-row bg-gradient-to-r from-gray-100 to-gray-200'>
           <div className='w-full md:w-1/2 flex items-center justify-center p-8 lg:p-12'>
           <div className='w-full max-w-md space-y-8 bg-white rounded-lg shadow-lg'>
             <div className='space-y-2 m-5'>
@@ -86,8 +87,21 @@ const Register=()=>{
         <Button type="submit">Submit</Button>
       </form>
     </Form>
+    <p className='p-2 text-center text-xl'>Already have  Account?
+      <Link href='/login' className='font-bold hover:text-blue-500'>Log in</Link>
+    </p>
   </div>
           </div>
+          </div>
+          <div className='hidden md:flex  w-full   p-12 items-center justify-center relative'>
+            
+            <div className='w-full h-full opacity-15 space-y-6 text-white bg-blue-50 z-10 absolute '>
+              <h2 className='text-4xl mt-72 text-black font-bold '>
+                Welcome to our Website.   
+              </h2>
+            </div>
+            
+              <img src="/coffe.jpg" alt="coffee" className='absolute inset-0 object-cover w-full h-full ' />
           </div>
         </div>
     )
