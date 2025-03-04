d579cbe747d50b52d65cdcb8c385afa3bc16179d
unknown <borshadevi27@gmail.com>
2025-03-04 23:38:06 +0600

    added user schema data

--- src/Models/User.jsx
+++ src/Models/User.jsx
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 
+
 const UserSchema = new mongoose.Schema({
     name:{
         type:String,
@@ -14,9 +15,16 @@ const UserSchema = new mongoose.Schema({
     password:{
         type:String,
         require: true,
+        select:false,
     },
-    
-
+    createdAt:{
+        type:Date,
+        default:Date.now,
+    },
+    isPremium:{
+        type:Boolean,
+        default: false,
+    },
 
 })
-const User=mongoose.model('User',UserSchema);
+export default mongoose.models.User ||  mongoose.model('User',UserSchema);
