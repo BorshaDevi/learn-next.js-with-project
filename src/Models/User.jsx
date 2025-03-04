a0df97e392806e096b1d5f6e39f13762eadc47c9
unknown <borshadevi27@gmail.com>
2025-03-04 14:21:54 +0600

    added text

--- src/Models/User.jsx
+++ src/Models/User.jsx
@@ -1,6 +1,22 @@
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        require: true,
+    },
+    email:{
+        type:String,
+        require: true,
+        unique:true,
+        lowercase:true,
+    },
+    password:{
+        type:String,
+        require: true,
+    },
+    
+
 
 })
 const User=mongoose.model('User',UserSchema);
