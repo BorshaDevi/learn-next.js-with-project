368a1c949f6b260f18b4ac9c3fa86daa3de3b8b8
unknown <borshadevi27@gmail.com>
2025-03-05 11:58:48 +0600

    added text

---
+++ src/action/register.js
@@ -0,0 +1,28 @@
+'use server'
+import {z} from 'zod'
+
+const formSchema=z.object({
+  name:z.string().min(2 ,{message:'Name must be 2 characters'}),
+  email:z.string().email({message: 'Email is required'}),
+  password:z.string().min(8 ,{
+    message:'Password must be 8 characters'
+  }),
+})
+
+export async function registerActionForm(formData){
+    const validation=formSchema.safeParse(
+        name:formData.get('name'),
+        email:formData.get('email'),
+        password:formData.get('password'),
+    )
+    try{
+
+    }catch(e){
+        console.log(e ,'Registration Error')
+        return{
+            message:'Internal Server Error',
+            status:500,
+        }
+    }
+
+}
