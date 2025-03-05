3ec5edd0f8ab9b66f04f111fa5e563fcbc43a5b7
unknown <borshadevi27@gmail.com>
2025-03-05 10:25:21 +0600

    create register file and added text in arcjet file

--- src/lib/arcjet.js
+++ src/lib/arcjet.js
@@ -1,7 +1,24 @@
-import arcjet from '@arcjet/next';
+import arcjet, { protectSignup } from '@arcjet/next';
 
 const aj=arcjet({
     key:process.env.ARCJET_KEY,
-    rules:[]
+    rules:[
+        protectSignup({
+            email:{
+                mode:'LIVE',
+                block:['DISPOSABLE', 'INVALID', 'NO_MX_RECORDS']
+            },
+            bots:{
+                mode:'LIVE',
+                allow:[],
+            },
+            rateLimit:{
+                mode:'LIVE',
+                interval:'1m',
+                max:2,
+            }
+
+        })
+    ]
 })
 export default aj;
