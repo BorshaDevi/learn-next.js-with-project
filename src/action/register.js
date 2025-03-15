1ce60dad1dba175b3f241c3b0cb7bbf473c3b364
unknown <borshadevi27@gmail.com>
2025-03-15 12:31:25 +0600

    added texts in register file

--- src/action/register.js
+++ src/action/register.js
@@ -43,10 +43,32 @@ export async function registerActionForm(formData){
           error :'Disposable email addresses are not allowed',
           status:403,
         }
+      }else if(emailTypes.includes("INVALID")){
+        return{
+          error :'Invalid email',
+          status:403,
+        }
       }else if(emailTypes.includes('NO_MX_RECORDS')){
         return{
-          
+          error :'Email domain does not have valid MX record',
+          status:403,
         }
+      }else{
+        return{
+          error :'Email address is not accepted. Please try again. ',
+          status:403,
+        }
+      }
+
+    }else if(decision.reason.isBot()){
+      return{
+        error:'Bot activity detected',
+        status : 403,
+      } 
+    }else if(decision.reason.isRateLimit()){
+      return {
+        error: 'Too many requests!Please try again later.',
+        status:429,
       }
     }
   }
