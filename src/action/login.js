f0613d5cd7a21c4238cb55855489c2cbfaf57e95
unknown <borshadevi27@gmail.com>
2025-03-18 21:45:15 +0600

    added texts in login.js file

--- src/action/login.js
+++ src/action/login.js
@@ -1,5 +1,5 @@
 'use server'
-
+import {z} from 'zod'
 
 
 
@@ -9,3 +9,24 @@ const formSchema=z.object({
     message:'Password must be 8 characters'
   }),
 })
+
+export async function loginActionForm(formData){
+  const validation = formSchema.safeParse(
+    {
+      email:formData.get('email'),
+      password:formData.get('password')
+    }
+  )
+
+
+if(!validation.success){
+  return{
+    error: 'validation Error',
+    status:400,
+  }
+}
+const {email , password} =validation.data
+
+
+
+}
