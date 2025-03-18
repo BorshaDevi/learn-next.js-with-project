d7883182937655564287754f1b9b5e9f253e8820
unknown <borshadevi27@gmail.com>
2025-03-18 12:53:52 +0600

    added texts

--- src/action/login.js
+++ src/action/login.js
@@ -1,1 +1,11 @@
 'use server'
+
+
+
+
+const formSchema=z.object({
+  email:z.string().email({message: 'Email is required'}),
+  password:z.string().min(8 ,{
+    message:'Password must be 8 characters'
+  }),
+})
