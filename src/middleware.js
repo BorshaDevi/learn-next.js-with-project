17d80c17019d49349e361d565f3fb40cf7814c35
unknown <borshadevi27@gmail.com>
2025-04-01 13:00:01 +0600

    added middleware logic

--- src/middleware.js
+++ src/middleware.js
@@ -1,12 +1,17 @@
 
 import { createMiddleware } from '@arcjet/next'
 import aj from './lib/arcjet'
+import { NextResponse } from 'next/server'
+
+
 export const config={
     matcher: ["/((?!_next/static|_next/image|favicon.ico|healthz).*)"],
 }
+
+
 const arcJetMiddleware= createMiddleware(aj)
 export async function middleware(request){
-    let arcJetResponse=arcJetMiddleware(request)
-    
+    const arcJetResponse=arcJetMiddleware(request)
+     const response=NextResponse.next()
     
 }
