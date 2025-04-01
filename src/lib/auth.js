a6148f836684fbe19fe6a07f806dbd0076d37a5d
unknown <borshadevi27@gmail.com>
2025-04-01 09:58:45 +0600

    jose jwtVerify logic

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,9 +1,11 @@
-export default function verifyAuth(token) {
+import { jwtVerify } from "jose";
+
+ export default async function verifyAuth(token) {
     if(!token){
         return null;
     }
     try{
-        
+        const {payload}=await jwtVerify(token , process.env.JWT_SECRET)
 
     }catch(e){
         console.log(e)
