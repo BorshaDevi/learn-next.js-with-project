5ca703140b568525ad7df91aa7c135e6571efe5a
unknown <borshadevi27@gmail.com>
2025-04-04 12:51:44 +0600

    Home component write

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -1,7 +1,7 @@
 const Home=()=>{
     return(
-     <div>
-        
+     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-20">
+        <p>Home</p>
      </div>
     )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,9 @@
+import Home from "@/Component/Home/Home";
+
 const  page=()=>{
   return(
     <div>
-      
+      <Home></Home>
     </div>
   )
 }
