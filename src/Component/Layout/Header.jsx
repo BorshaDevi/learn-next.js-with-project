272a9108dd836c5376ab5f1b5dc3a2c6d051e721
unknown <borshadevi27@gmail.com>
2025-04-05 12:27:48 +0600

    Header file create router

--- src/Component/Layout/Header.jsx
+++ src/Component/Layout/Header.jsx
@@ -1,15 +1,18 @@
-import { Button } from "@/components/ui/button";
+
+'use client'
 import { Input } from "@/components/ui/input";
+import { useRouter } from "next/navigation";
 import { CiSearch } from "react-icons/ci";
 import { MdEditNote } from "react-icons/md";
 const Header=()=>{
+  const router = useRouter()
     return(
           <div className="fixed top-0 left-0 right-0 bg-yellow-100  z-50">
           <div className="border-b">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                <div className="flex items-center justify-between h-16">
                 <div className="flex items-center">
-                    <h1 className="text-2xl cursor-pointer font-bold font-serif tracking-tight">
+                    <h1  onClick={()=> router.push('/')} className="text-2xl cursor-pointer font-bold font-serif tracking-tight">
                       <span className="bg-black rounded-full text-white px-2 py-1  border border-yellow-500">
                         M
                       </span>
@@ -27,9 +30,9 @@ const Header=()=>{
                      />
                     <CiSearch className="absolute -translate-y-7 transform right-3 cursor-pointer "/>
                   </div>
-                  <Button>
-                   <MdEditNote  />
-                  </Button>
+                  
+                   <MdEditNote onClick={()=> router.push('/blog/create')} className="text-2xl" />
+                  
                 </div>
                </div>
             </div>
