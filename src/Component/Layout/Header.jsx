aaec3956d274cbb5e6ca3845217a43aa8d30a066
unknown <borshadevi27@gmail.com>
2025-04-05 10:37:53 +0600

    added text

--- src/Component/Layout/Header.jsx
+++ src/Component/Layout/Header.jsx
@@ -1,3 +1,5 @@
+import { Input } from "@/components/ui/input";
+import { CiSearch } from "react-icons/ci";
 const Header=()=>{
     return(
           <div className="fixed top-0 left-0 right-0 bg-yellow-100  z-50">
@@ -12,8 +14,18 @@ const Header=()=>{
                       <span className="ml-1">
                         Marg Ltd
                       </span>
-                      
                     </h1>
+                </div>
+                <div className="flex items-center space-x-4">
+                  <div className="relative hidden md:block ">
+                     <Input
+                      type="text"
+                     placeholder="Search blogs..."
+                     className="w-full h-10 px-4 py-2 border border-gray-100 bg-white rounded-md focus:outline-none focus:ring focus:border-yellow-200"
+                     />
+                     <CiSearch className=""/>
+                  </div>
+
                 </div>
                </div>
             </div>
