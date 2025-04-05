9b863f91ddf5edb5d8d485bb287d5e1dfb2cad27
unknown <borshadevi27@gmail.com>
2025-04-05 12:16:28 +0600

    added text

--- src/Component/Layout/Header.jsx
+++ src/Component/Layout/Header.jsx
@@ -1,5 +1,7 @@
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { CiSearch } from "react-icons/ci";
+import { MdEditNote } from "react-icons/md";
 const Header=()=>{
     return(
           <div className="fixed top-0 left-0 right-0 bg-yellow-100  z-50">
@@ -18,14 +20,16 @@ const Header=()=>{
                 </div>
                 <div className="flex items-center space-x-4">
                   <div className="relative hidden md:block ">
-                    <CiSearch className="absolute mt-3"/>
                      <Input
                       type="text"
                      placeholder="Search blogs..."
                      className="w-full pl-10 pr-4  h-10 px-4 py-1 border border-gray-100 bg-white rounded-md focus:outline-none focus:ring focus:border-yellow-200"
                      />
+                    <CiSearch className="absolute -translate-y-7 transform right-3 cursor-pointer "/>
                   </div>
-
+                  <Button>
+                   <MdEditNote  />
+                  </Button>
                 </div>
                </div>
             </div>
